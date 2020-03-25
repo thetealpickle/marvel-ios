f4711527fef161eef438642b7fdaa42a14557462
Jessica Joseph <jessjeanjo@gmail.com>
2020-03-25 12:43:52 -0400

    ⚡️ the beginning of a mini party 🎉

---
+++ marvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  marvel
+//
+//  Created by Jessica Joseph on 3/25/20.
+//  Copyright © 2020 JESSICA JEAN JOSEPH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
